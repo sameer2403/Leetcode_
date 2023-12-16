8124757b41128ebf77ee71f8ef2932ac6884c46e
Sameer Suman <76219383+sameer2403@users.noreply.github.com>
2023-12-17 00:52:44 +0530

    Time: 3 ms (97.78%), Space: 7.8 MB (45.96%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+         if (s.size()!=t.size()) return false;
+        unordered_map <char, int> umap;
+        for (int i=0; i<s.size(); i++) {
+            umap[s[i]]++;
+            umap[t[i]]--;
+        }
+        for (auto it: umap) {
+            if (it.second !=0) return false;
+        }
+        return true;
+    }
+};
